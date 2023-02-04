8af0fd204b9a817cded2c54a580752ec18b5f93a
theresmaryjohn <theresmaryjohn@gmail.com>
2023-02-04 14:49:33 +0530

    feat:js 34 projects

--- 50 projects js/00032/script.js
+++ 50 projects js/00032/script.js
@@ -7,6 +7,16 @@ toggles.forEach(toggle=> toggle.addEventListener('change', (e)=>
 doTheTrick(e.target)))
 
 function doTheTrick(theClickedOne){
-    if(good.checked && cheap.checked)
+    if(good.checked && cheap.checked && fast.checked){
+        if(good === theClickedOne){
+            fast.checked = false
+        }
+        if(cheap === theClickedOne){
+            good.checked = false
+        }
+        if(fast === theClickedOne){
+            cheap.checked = false
+        }
+    }
 
 }
